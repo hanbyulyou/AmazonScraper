a17b18c2e67c75a49b8345b841459cb007327ba1
hanbyulyou <111835703+hanbyulyou@users.noreply.github.com>
2023-12-14 12:32:48 +0900

    Update az_read-data.py
    Add descrption => save to excel and csv

--- az_read-data.py
+++ az_read-data.py
@@ -7,3 +7,8 @@ conn = sqlite3.connect('amztracker.db')
 df = pd.read_sql_query('''SELECT * FROM prices''', conn)
 
 print(df)
+
+
+#save df to csv fiel and excel file
+df.to_csv('azscraptable.csv')
+df.to_excel('azscraptable.xlsx')
